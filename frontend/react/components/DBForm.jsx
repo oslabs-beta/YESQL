c9fcdd9004c31094b1b59b7b21699689fc559fbc
TaylorBC <taylor.brian99@yahoo.com>
2024-03-02 15:02:21 -0700

    working on dbFrom

--- frontend/react/components/DBForm.jsx
+++ frontend/react/components/DBForm.jsx
@@ -1,1 +1,18 @@
-import React from 'react';
+import React from "react";
+const DBForm = () => {
+    return ( 
+        <form action='/credentials' method="POST"> 
+          <label for="user">USER</label> 
+          <input type="text" id="user"></input>
+          <label for="host">Host</label> 
+          <input type="text" id="host"></input> 
+          <label for="database">database</label> 
+          <input type="text" id="database"></input> 
+          <label for="port">port</label> 
+          <input type="text" id="port"></input> 
+          <input type = "submit" value = "submit"></input>
+        </form>
+     );
+}
+ 
+export default DBForm;
--- frontend/react/containers/Landing.jsx
+++ frontend/react/containers/Landing.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import LandingPageText from '../components/LandingPageText.jsx';
+import LandingPageText from '../components/LandingPageText.jsx'; 
+import DBForm from '../components/DBForm.jsx';
 
 const Landing = () => {
   return (
     <div>
-      <LandingPageText/>
+      <LandingPageText/> 
+      <DBForm/>
     </div>
   )
 }
