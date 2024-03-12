82f2b06bc9d708608ab8fdad66f318398d661e0e
Nina Skyttmo <ninaskyttmo@Ninas-MacBook-Pro-2.local>
2024-03-12 16:52:57 -0400

    modified form POST request and redirect

--- frontend/react/components/DBForm.jsx
+++ frontend/react/components/DBForm.jsx
@@ -1,9 +1,41 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const DBForm = () => {
+    const navigate = useNavigate();
+
+    const handleSubmit = (event) => {
+      event.preventDefault();
+
+      const formData = {
+        user: event.target.elements.user.value,
+        host: event.target.elements.host.value,
+        database: event.target.elements.database.value,
+        port: event.target.elements.port.value,
+      }
+      
+      fetch('/connect', {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          user: formData.user, 
+          host: formData.host, 
+          database: formData.database, 
+          port: formData.port
+        })
+      })
+      .then(response => response.json())
+      .then(data => {
+        console.log('we got something back!', data)
+      })
+      .catch(error => console.log('we have an error', error));
+
+      navigate('/chart');
+    }
     return ( 
-        <form action="/connect" method="POST"> 
+        <form onSubmit={handleSubmit}> 
           <h2>Database Credentials</h2>
           <label htmlFor="user">USER</label> 
           <input type="text" name="user" id="user"></input>
