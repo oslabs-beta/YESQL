e4f0ba5681bcf0ec4a25954a7dd3167a9e202d5c
Hubbisand <Benjamin.Reid.Jackson@gmail.com>
2024-04-01 20:41:22 -0400

    Queried foreign keys and added edges

--- backend/controllers/databaseController.js
+++ backend/controllers/databaseController.js
@@ -21,20 +21,31 @@ databaseController.query = async (req, res, next) => {
         const tables = await db.query(`SELECT table_name FROM information_schema.tables WHERE table_schema = 'public' AND table_type = 'BASE TABLE'`);
         const outputObject = {};
         for (let i = 0; i < tables.rows.length; i++) {
+            outputObject.tableName = tables[i];
             const outputArray = [];
             const query = `SELECT column_name FROM information_schema.columns WHERE table_name = '${tables.rows[i].table_name}'`
             const columns = await db.query(query);
-            for (let i = 0; i < columns.rows.length; i++) {
-                outputArray.push(columns.rows[i].column_name);
+            for (let j = 0; j < columns.rows.length; j++) {
+                outputArray.push(columns.rows[j].column_name);
             };
-            outputObject[tables.rows[i].table_name] = outputArray;
+            outputObject[tables.rows[i].table_name] = {columns: outputArray};
+        };
+        // This section is for determining and connectiong foreign and primary keys
+        const connections = await db.query('SELECT constraint_name, unique_constraint_name FROM information_schema.referential_constraints');
+        for (let i = 0; i < connections.rows.length; i++) {
+            const primary = connections.rows[i].unique_constraint_name.slice(0, -3);
+            const foreign = connections.rows[i].constraint_name.slice(0, connections.rows[i].constraint_name.search(/(_fk)/gm));
+            if (!outputObject[foreign].connections) outputObject[foreign].connections = [primary];
+            else outputObject[foreign].connections.push(primary);
         };
         res.locals.tableData = outputObject;
+        console.log('Output Object => ', outputObject);
         return next();
     } catch (error) {
         console.log(error);
         return next(error);
     };
 };
+
 
 module.exports = databaseController;
