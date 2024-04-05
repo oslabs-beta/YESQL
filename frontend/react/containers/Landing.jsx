55f9adc6ba347ad5d0b277581663f6ab6bd8ebbd
Nina Skyttmo <ninaskyttmo@Ninas-MacBook-Pro-2.local>
2024-04-04 22:42:20 -0400

    eslint installed

--- frontend/react/containers/Landing.jsx
+++ frontend/react/containers/Landing.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import LandingPageText from '../components/LandingPageText.jsx'; 
-import DBForm from '../components/DBForm.jsx'; 
-import { Link } from 'react-router-dom';
+import LandingPageText from '../components/LandingPageText.jsx';
+import {Link} from 'react-router-dom';
 
 const Landing = () => {
   return (
@@ -10,26 +9,26 @@ const Landing = () => {
       {/* <h3>
         make your queries optimal with <span className="yesql-intro">Yes<span>ql</span></span></h3> */}
       <div className="landing-page-grid">
-      <div> 
-      {/* render instructions: */}
-        <LandingPageText/> 
-      </div>
-      <div>
+        <div>
+          {/* render instructions: */}
+          <LandingPageText/>
+        </div>
+        <div>
           <div className="landing-gradient"></div>
           <div className="landing-gradient-2"></div>
           <h3><span>
             In a world where time is currency,
             <br />optimize SQL queries effortlessly with Yesql.
           </span>
-            <br/>Visualize records, columns, and table segments to 
-            <br/>craft finely tuned queries for optimal performance. </h3>
-            {/*link tag points to /formpage */}
-        <Link className="button" to='/formpage'>Connect to local Database</Link>
-        <Link className='button' to='/hostedFormPage'>Connect to hosted Database</Link>
-      </div>
+          <br/>Visualize records, columns, and table segments to
+          <br/>craft finely tuned queries for optimal performance. </h3>
+          {/* link tag points to /formpage */}
+          <Link className="button" to='/formpage'>Connect to local Database</Link>
+          <Link className='button' to='/hostedFormPage'>Connect to hosted Database</Link>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Landing;
