422393e14570d1ef7301560cd09bb0ebc0f8f1c9
Nina Skyttmo <ninaskyttmo@Ninas-MacBook-Pro-2.local>
2024-04-04 20:33:08 -0400

    fixed bug

--- frontend/querySlice.js
+++ frontend/querySlice.js
@@ -50,17 +50,18 @@ const querySlice = createSlice({
           indexOfFrom = index;
         }
       });
-      console.log(indexOfFrom, ' <--- indexOfFrom in add reducer')
       const isParentIncluded = state.query.some(el => el.string === action.payload.parent);
       const isConnection = state.query.some(el => connections ? connections.includes(el.string) : false);
       if (action.payload.parent && !isParentIncluded) {
         if (fromIncluded && !isConnection) alert('Tables do not have a connection!');
       };
       if (fromIncluded && action.payload.parent !== 'clause' && action.payload.parent) {
-        if (isConnection || isParentIncluded) {
+        if (state.numOfClauses > 2) {
+          state.query.push(action.payload);
+        } else if (isConnection || isParentIncluded) {
           state.query.splice(indexOfFrom, 0, action.payload)
           state.numOfColumns++;
-          console.log(state.numOfColumns, '<--- numOfColumns')
+          console.log(state.numOfColumns, '<--- numOfColumns met the condition')
         } 
       } else {
         // console.log('Payload => ', action.payload);
@@ -73,7 +74,7 @@ const querySlice = createSlice({
         state.numOfClauses++;
         state.query.push({ string: action.payload.parent, parent: action.payload.parent });
       };
-      if (length > 2 && indexOfFrom < length - 1 && state.numOfColumns > 1) {
+      if (length > 2 && indexOfFrom < length - 1 && state.numOfColumns === 2) {
         for (let i = 1; i < indexOfFrom; i++) {
             if (!state.query[i].string.includes(',')) state.query[i].string += ',';
         }
@@ -82,25 +83,23 @@ const querySlice = createSlice({
     },
     // In the remove reducer, we are filtering out the object that we receive through the payload. 
     remove(state, action) {
-      // console.log(action.payload);
-      // if (typeof action.payload.string === 'string' && action.payload.string.endsWith(',')) {
-      //   action.payload.string = action.payload.string.slice(0, -1);
-      // }
-      let hasComma;
+      let stringWithComma;
       state.removedNode = action.payload;
-      state.query = state.query.filter((node, index) => {
-        if (typeof node.string === 'string' && node.string.endsWith(',')) {
-          hasComma = node.string.slice(0, -1)
+      state.query = state.query.filter((node) => {
+        if (typeof node.string === 'string' && node.string.endsWith(',') && node.string === action.payload.string) {
+          stringWithComma = node.string.slice(0, -1)
         }
-        return !(node.string === action.payload.string || hasComma === action.payload.string && node.parent === action.payload.parent)
-      })
-      let indexOfFrom;
+        return !(node.string === action.payload.string || stringWithComma === action.payload.string && node.parent === action.payload.parent)
+      });
+      const includesFrom = state.query.some(item => item.string === 'FROM');
+      if (includesFrom && state.query.length < 4 && state.query.length > 2) {
+        state.query = state.query.slice(0, -2);
+      }
       for (let i = 0; i < state.query.length; i++) {
-        if (state.query[i].string === 'FROM' && i > 1) {
+        if (state.query[i].string === 'FROM' && i > 1 && state.query[i].string.endsWith(',')) {
           state.query[i - 1].string = state.query[i - 1].string.slice(0, -1);
         }
       }
-  
     },
     // We are adding a clause through the addClause reducer. We want to make 
     // sure that the added clause isn't an '=' or an '*'. 
