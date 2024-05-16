06b8c2c19fd81500ca1637002e00aee97da1e84a
Hubbisand <benjamin.reid.jackson@gmail.com>
2024-05-15 21:38:07 -0400

    Router update 4

--- frontend/index.js
+++ frontend/index.js
@@ -4,14 +4,17 @@ import App from './App.jsx';
 import './styles/index.scss';
 import {store} from './store';
 import {Provider} from 'react-redux';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 
 root.render(
+  <HashRouter>
     <Provider store={store}>
       <App/>
     </Provider>,
+  </HashRouter>
 );
 
 
